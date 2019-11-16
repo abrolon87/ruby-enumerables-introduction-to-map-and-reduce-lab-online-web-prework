b990f97cb5c5a2ec38804832ef24a5167aa6e60e
Amanda Rolon <abrolon87@gmail.com>
2019-11-16 18:19:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,12 +50,10 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
-
   counter = 0
   while counter < source_array.length do
-
-    if source_array[counter]
-    return true
+    if !source_array[counter]
+    return false
     
     counter += 1
   end
